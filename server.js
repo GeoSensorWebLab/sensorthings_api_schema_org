9d36aa598aeae4b8fff9a5d12a781834222e4e53
James Badger <jpbadger@ucalgary.ca>
2020-02-21 09:46:44 -0700

    Add default pug index page

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+app.set('view engine', 'pug');
+
 app.use(express.static('public'));
 
+app.get('/', function (req, res) {
+  res.render('index', { message: 'Hello there!' });
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+  console.log(`Example app listening on port ${port}!`);
 });
